16e3776bc95980fc5a8b864b001b5c598e1b76f9
yuanshuai <yuanshuai@gyyx.cn>
2023-03-26 11:13:22 +0800

    项目目录重构

--- aibot-server/src/main/java/com/chatgpt/ChatgptApplication.java
+++ aibot-server/src/main/java/com/chatgpt/ChatgptApplication.java
@@ -2,7 +2,6 @@ package com.chatgpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
