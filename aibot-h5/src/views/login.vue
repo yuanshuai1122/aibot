1197d68944aae6d499efdb3499661fa9bbf9b124
wangxinru <2075427679@qq.com>
2023-03-30 23:15:58 +0800

    注册登录样式

--- aibot-h5/src/views/login.vue
+++ aibot-h5/src/views/login.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="login-content">
+  <div >
+    <div style="color: #f8f8f8 ">您好，</div>
+    <div style="color: #f8f8f8 ">欢迎来到云网AI</div>
+    <div class="login-content">
     <van-cell-group class="content">
       <van-field class="field" v-model="logForm.account" label="账号" placeholder="请输入账号" />
       <van-field class="field" v-model="logForm.password" label="密码" placeholder="请输入密码" />
     </van-cell-group>
+    <div style="display: flex;justify-content: center;align-items: center;margin-top: 2vh">
+      <van-radio-group v-model="radio" @change="changeRadio">
+        <van-radio name='1' icon-size="2vh" checked-color="#3398FF" @click="agreeBtn('1')"></van-radio>
+      </van-radio-group>
+      <!--        <van-checkbox v-model="radio" checked-color="#3398FF" :label-disabled="true" shape="square" icon-size="15px"/>-->
+      <span style="font-size: 1vh;color: #CACACA">我已阅读并同意《用户协议》《xxxx》和《隐私政策》</span>
+    </div>
     <van-button class="submit-btn" round type="success" @click="loginBtn">登录</van-button>
+    <div style="display: flex;justify-content: space-around;width: 100%;margin-top: 2vh">
+      <div style="font-size: 1vh;float: left;color: #CACACA">验证码登录</div>
+      <div style="font-size: 1vh;float: right;color: #CACACA" @click="toRegister">新用户注册</div>
+    </div>
+    </div>
   </div>
 
 </template>
@@ -21,9 +36,21 @@ export default {
       account: "",
       password: ""
       },
+      radio:"",
+      radioChange:false
     }
   },
   methods:{
+    changeRadio(){
+      this.radioChange=true
+    },
+    agreeBtn(e){
+      console.log(this.radio)
+      if(!this.radioChange){
+        this.radio=''
+      }
+      this.radioChange=false
+    },
   //  提交表单
     loginBtn(){
       console.log(this.logForm)
@@ -38,6 +65,12 @@ export default {
         }
       })
     },
+  //  去注册
+    toRegister(){
+      this.$router.push({
+        path:'/register'
+      })
+    }
   },
 }
 </script>
@@ -51,18 +84,42 @@ export default {
   align-items: center;
   justify-content: center;
   background-size: cover;
-  background: url(@/assets/login.png) no-repeat fixed;
+  background: url(@/assets/bg.png) no-repeat fixed #333333;
 }
 .content{
+  width: 80%;
   background: none;
   /*background-color: red;*/
 }
 .field{
+  width: 100%;
+  background-color: #212230 ;
+  margin-top: 3vh;
+  border: 0.1vh solid black;
+  border-radius: 3vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.van-field__control{
+  color: white;
+}
+.van-cell-group{
   background: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.van-cell::after{
+  border-bottom: 0;
+}
+.van-hairline--top-bottom::after{
+  border-width: 0;
 }
 .submit-btn{
   margin-top: 2vh;
-  width: 20vh;
+  width: 80%;
   background-color: steelblue;
 }
 
--- aibot-h5/src/views/register.vue
+++ aibot-h5/src/views/register.vue
@@ -1,27 +1,35 @@
 <template>
     <div class="login-content">
       <van-cell-group class="content" :ref="rules">
-        <van-field class="field" v-model="logForm.account" label="手机号" placeholder="请输入11位手机号" />
-        <van-field class="field" v-model="logForm.password" label="密码" placeholder="请输入验证码">
+        <van-field label-width="8vh" class="field" v-model="logForm.account" label="手机号" placeholder="请输入11位手机号" />
+        <van-field label-width="8vh" class="field" v-model="logForm.password" label="密码" placeholder="请输入验证码">
           <template #button>
-            <van-button size="small" type="text" @click="sendCode">发送验证码</van-button>
+            <van-button
+              size="small"
+              native-type="button"
+              class="btn"
+              @click="sendCode"
+            >
+              获取验证码
+            </van-button>
+<!--            <van-button size="small" type="text" @click="sendCode">发送验证码</van-button>-->
           </template>
         </van-field>
-        <van-field class="field" v-model="logForm.shareCode" label="邀请码" placeholder="请输入登录密码" />
-        <van-field class="field" v-model="logForm.account" label="账号" placeholder="请再次输入登录密码" />
-        <van-field class="field" v-model="logForm.password" label="密码" placeholder="请输入邀请码（选填）" />
+        <van-field label-width="8vh" class="field" v-model="logForm.shareCode" label="邀请码" placeholder="请输入登录密码" />
+        <van-field label-width="8vh" class="field" v-model="logForm.account" label="账号" placeholder="请再次输入登录密码" />
+        <van-field label-width="8vh" class="field" v-model="logForm.password" label="密码" placeholder="请输入邀请码（选填）" />
       </van-cell-group>
       <div style="display: flex;justify-content: center;align-items: center;margin-top: 2vh">
-        <van-radio-group v-model="radio">
-        <van-radio name="1" icon-size="2vh" checked-color="#3398FF"></van-radio>
+        <van-radio-group v-model="radio" @change="changeRadio">
+          <van-radio name='1' icon-size="2vh" checked-color="#3398FF" @click="agreeBtn('1')"></van-radio>
         </van-radio-group>
-<!--        <van-checkbox v-model="radio" checked-color="#3398FF" :label-disabled="true" shape="square" icon-size="15px"/>-->
-        <span style="font-size: 1vh">我已阅读并同意《用户协议》《xxxx》和《隐私政策》</span>
+        <!--        <van-checkbox v-model="radio" checked-color="#3398FF" :label-disabled="true" shape="square" icon-size="15px"/>-->
+        <span style="font-size: 1vh;color: #CACACA">我已阅读并同意《用户协议》《xxxx》和《隐私政策》</span>
       </div>
 
       <van-button class="submit-btn" round type="success" @click="registerBtn">注册</van-button>
       <div style="font-size: 1vh;margin-top: 2vh">
-      <span>已有账号？</span>
+      <span style="color: #CACACA">已有账号？</span>
       <span style="color: #1989fa" @click="toLogin">直接登录</span>
       </div>
     </div>
@@ -35,12 +43,13 @@
       name: 'register',
       data(){
         return{
-          radio:'1',
           logForm:{
             account: '',
             password: '',
             shareCode:''
           },
+          radio:"",
+          radioChange:false,
           rules:{
             account: [{ required: true, message: '账号必须填写！', trigger: ['blur', 'change'] }],
             password: [{ required: true, message: '密码必须填写！', trigger: ['blur', 'change'] }],
@@ -48,6 +57,16 @@
         }
       },
       methods:{
+        changeRadio(){
+          this.radioChange=true
+        },
+        agreeBtn(e){
+          console.log(this.radio)
+          if(!this.radioChange){
+            this.radio=''
+          }
+          this.radioChange=false
+        },
         //  提交表单
         registerBtn(){
           console.log(this.logForm)
@@ -86,7 +105,8 @@
       align-items: center;
       justify-content: center;
       background-size: cover;
-      background: url(@/assets/login.png) no-repeat fixed;
+      /*background-color: #333333;*/
+      background: url(@/assets/bg.png) no-repeat fixed #333333;
     }
     .content{
       background: none;
@@ -113,16 +133,23 @@
     .field{
 
       width: 80%;
-      background: none;
+      /*background: none;*/
+      background-color: #212230 ;
       margin-top: 3vh;
       border: 0.1vh solid black;
       border-radius: 3vh;
+      vertical-align: center;
+      display: flex;
+      align-items: center;
     }
 
     .submit-btn{
       margin-top: 2vh;
-      width: 20vh;
+      width: 70%;
       background-color: steelblue;
+    }
+    .btn{
+      height: 3.5vh;
     }
     /*@field-label-width{*/
     /*  width: 6vh;*/
